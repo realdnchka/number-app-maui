43591f1ea9b3003478758060f86aa67e76b4ff36
Danil Chernov <realdnchka@gmail.com>
2023-10-10 04:20:27 +0400

    init

---
+++ NumberApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NumberApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ NumberApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NumberApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
